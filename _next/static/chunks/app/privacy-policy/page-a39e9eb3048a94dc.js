344c5ec2370757ff68f4859c5934414e44493d5b
RomanTarasenko <67711196+RomanTarasenko@users.noreply.github.com>
2025-10-18 00:08:07 +0200

    Updates

---
+++ _next/static/chunks/app/privacy-policy/page-a39e9eb3048a94dc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[902],{4881:(e,s,_)=>{Promise.resolve().then(_.bind(_,1684))}},e=>{var s=s=>e(e.s=s);e.O(0,[486,303,441,684,358],()=>s(4881)),_N_E=e.O()}]);
